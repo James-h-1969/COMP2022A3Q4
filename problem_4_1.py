712b614461a9441b250c92cffc015b8ab450acc5
James Hocking <jhoc6907@uni.sydney.edu.au>
2023-10-10 14:51:00 +1100

    tst

--- problem_4_1.py
+++ problem_4_1.py
@@ -7,7 +7,7 @@ cat "4.1.tm " | python problem_4_1.py
 ZERO = "0"
 STAR = "*"
 
-def go_to_furthest(num:str, direction:str):
+def go_to_furthest(num:str, direction:str, final_state:str):
     if (direction == "L" or direction == 'l'):
         new_direction = "R"
     else:
@@ -19,7 +19,7 @@ def go_to_furthest(num:str, direction:str):
               [num+"a", "2", "2", new_direction, num+"a"],
               [num+"a", "3", "3", new_direction, num+"a"]]
     actual_end = [num+"a", "_", "_", direction, num+"b"]
-    find_new_zero = get_to_a_zero(num+"b", direction, "-1")
+    find_new_zero = get_to_a_zero(num+"b", direction, final_state, 1)
     final_lines.append(to_go)
     for line in to_end:
         final_lines.append(line)
@@ -28,21 +28,21 @@ def go_to_furthest(num:str, direction:str):
     final_lines.append(actual_end)
     return final_lines
 
-def get_to_a_zero(num:str, direction:str, final_state:str):
+def get_to_a_zero(num:str, direction:str, final_state:str, depth:int):
     final_lines = []
     keep_moving = [[num, "1", "1", direction, num], 
                    [num, "2", "2", direction, num], 
                    [num, "3", "3", direction, num], ]
-    if (final_state != "-1"):
-        gets_to_end = go_to_furthest(num, direction)
+    if (depth == 0):
+        gets_to_end = go_to_furthest(num, direction, final_state)
         for line in gets_to_end:
             final_lines.append(line)
     else:
         final_lines.append([num, "_", "_", "L", "halt-reject"])
-    find_zero = [num, "0", "0", "*", final_state]
-    final_lines.append(find_zero)
     for line in keep_moving:
         final_lines.append(line)
+    find_zero = [num, "0", "0", "*", final_state]
+    final_lines.append(find_zero)
     return final_lines
 
 
@@ -57,7 +57,7 @@ if __name__ == '__main__':
         if input_letter == ZERO or input_letter == STAR:
             # follow direction
             final_lines.append([start_state, input_letter, output_letter, direction, str(counter)])
-            new_rules = get_to_a_zero(str(counter), direction, output_state)
+            new_rules = get_to_a_zero(str(counter), direction, output_state, 0)
             counter += 1
             for line in new_rules:
                 final_lines.append(line)
@@ -66,8 +66,8 @@ if __name__ == '__main__':
 
 
     # if you use the same data structure, you can use:
-    # print("Initial TM:")
-    # printTM(lines)
-    # print(" ")
-    # print("Final PTM:")
+    print("Initial TM:")
+    printTM(lines)
+    print(" ")
+    print("Final PTM:")
     printTM(final_lines)
