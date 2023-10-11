8613ffd3b54d251962110b7e566c66e1e515ffe0
James Hocking <jhoc6907@uni.sydney.edu.au>
2023-10-11 17:20:40 +1100

    test

--- problem_4_1.py
+++ problem_4_1.py
@@ -1,7 +1,8 @@
 from TMparser import parseInput, printTM
 
 """
-cat "4.1.tm " | python problem_4_1.py
+TO EXECUTE THE PROGRAM:
+    cat "4.1.tm " | python problem_4_1.py
 """
 
 ZERO = "0"
@@ -10,22 +11,24 @@ STAR = "*"
 UNDERLINE = "_"
 REPLACEMENT_LETTER = "Z"
 
+"""
+checkifExists: a function to check whether a rule has already been written
+@params: lines -> rules already created, start_state -> start of rule, input -> letter of rule
+@ returns boolean: if already created
+"""
 def checkifExists(lines, start_state, input):
     for line in lines:
         if start_state == line[0] and input == line[1]:
             return False
     return True
 
-if __name__ == '__main__':
+
+def main():
     # read an TM via stdin. See parser.py for details on the returned object
     lines = parseInput()
     final_lines = []
-    # Put your code here:
-    counter = 10
-
     for line in lines:
         start_state, input_letter, output_letter, direction, output_state = line
-        
         if output_letter == ZERO or output_letter == STAR:
             if output_letter == ZERO:
                 final_lines.append([start_state, input_letter, REPLACEMENT_LETTER, direction, output_state])
@@ -41,19 +44,11 @@ if __name__ == '__main__':
                     final_lines.append([start_state, REPLACEMENT_LETTER, REPLACEMENT_LETTER, direction, output_state])
         else:
             final_lines.append(line)
+        if input_letter == ZERO and output_letter != ZERO:
+            final_lines.append([start_state, REPLACEMENT_LETTER, output_letter, direction, output_state])
 
-        if input_letter == ZERO:
-            if output_letter == ZERO:
-                final_lines.append([start_state, REPLACEMENT_LETTER, REPLACEMENT_LETTER, direction, output_state])
-            else:
-                final_lines.append([start_state, REPLACEMENT_LETTER, output_letter, direction, output_state])
-
-    
-    # print("Initial TM:")
-    # printTM(lines)
-    # print(" ")
-    # print("Final PTM:")
     printTM(final_lines)
 
     
-
+if __name__ == '__main__':
+    main()
--- problem_4_2.py
+++ problem_4_2.py
@@ -1,10 +1,29 @@
 from TMparser import parseInput, printTM
 
+"""
+TO EXECUTE THE PROGRAM:
+    cat "4.2.ptm " | python problem_4_2.py
+"""
+
 ZERO = "0"
 ONE = "1"
+UNDERLINE = "_"
 STAR = "*"
+SUBSTITUTE_LETTER = "Z"
 
-def go_to_furthest(num:str, direction:str, final_state:str):
+"""
+checkifExists: a function to check whether a rule has already been written
+@params: lines -> rules already created, start_state -> start of rule, input -> letter of rule
+@ returns boolean: if already created
+"""
+def checkifExists(lines, start_state, input):
+    for line in lines:
+        if start_state == line[0] and input == line[1]:
+            return False
+    return True
+
+
+def go_to_furthest(num:str, direction:str, final_state:str, output_letter:str):
     if (direction == "L" or direction == 'l'):
         new_direction = "R"
     else:
@@ -13,10 +32,9 @@ def go_to_furthest(num:str, direction:str, final_state:str):
     to_go = [num, "_", "_", new_direction, num+"a"]
     to_end = [[num+"a", "0", "0", new_direction, num+"a"],
               [num+"a", "1", "1", new_direction, num+"a"],
-              [num+"a", "2", "2", new_direction, num+"a"],
-              [num+"a", "3", "3", new_direction, num+"a"]]
+              [num+"a", SUBSTITUTE_LETTER, SUBSTITUTE_LETTER, new_direction, num+"a"],]
     actual_end = [num+"a", "_", "_", direction, num+"b"]
-    find_new_zero = get_to_a_zero(num+"b", direction, final_state, 1)
+    find_new_zero = get_to_a_zero(num+"b", direction, final_state, 1, output_letter)
     final_lines.append(to_go)
     for line in to_end:
         final_lines.append(line)
@@ -25,21 +43,16 @@ def go_to_furthest(num:str, direction:str, final_state:str):
     final_lines.append(actual_end)
     return final_lines
 
-def get_to_a_zero(num:str, direction:str, final_state:str, depth:int):
+def get_to_a_zero(num:str, direction:str, final_state:str, depth:int, output_letter:str):
     final_lines = []
-    keep_moving = [[num, "1", "1", direction, num], 
-                   [num, "2", "2", direction, num], 
-                   [num, "3", "3", direction, num], ]
+    final_lines.append([num, "1", "1", direction, num])
     if (depth == 0):
-        gets_to_end = go_to_furthest(num, direction, final_state)
+        gets_to_end = go_to_furthest(num, direction, final_state, output_letter)
         for line in gets_to_end:
             final_lines.append(line)
-    # else:
-    #     final_lines.append([num, "_", "_", "L", "halt-reject"])
-    for line in keep_moving:
-        final_lines.append(line)
-    find_zero = [num, "0", "0", "*", final_state]
-    final_lines.append(find_zero)
+    else:
+        final_lines.append([num, SUBSTITUTE_LETTER, output_letter, "*", final_state])
+    final_lines.append([num, ZERO, ZERO, "*", final_state])
     return final_lines
 
 
@@ -52,28 +65,27 @@ if __name__ == '__main__':
     for line in lines:
         start_state, input_letter, output_letter, direction, output_state = line
         if input_letter == ZERO or input_letter == STAR:
+            
             # follow direction
             if input_letter == ZERO:            
-                final_lines.append([start_state, input_letter, output_letter, direction, str(counter)])
-                new_rules = get_to_a_zero(str(counter), direction, output_state, 0)
+                final_lines.append([start_state, ZERO, SUBSTITUTE_LETTER, direction, str(counter)])
+                new_rules = get_to_a_zero(str(counter), direction, output_state, 0, output_letter)
                 counter += 1
                 for line in new_rules:
                     final_lines.append(line)
+            
+          #############  HAPPY ABOVE  ############  
             else:
-                final_lines.append([start_state, ZERO, ZERO, direction, str(counter)])
-                final_lines.append([start_state, ONE, ONE, direction, output_state])
-                new_rules = get_to_a_zero(str(counter), direction, output_state, 0)
+                if checkifExists(final_lines, start_state, ZERO):
+                    final_lines.append([start_state, ZERO, ZERO, direction, str(counter)])
+                if checkifExists(final_lines, start_state, ONE):
+                    final_lines.append([start_state, ONE, ONE, direction, output_state])
+                new_rules = get_to_a_zero(str(counter), direction, output_state, 0, output_letter)
                 counter += 1
                 for line in new_rules:
                     final_lines.append(line)
         else:
             final_lines.append(line)
 
-
-    # if you use the same data structure, you can use:
-    # print("Initial TM:")
-    # printTM(lines)
-    # print(" ")
-    # print("Final PTM:")
     printTM(final_lines)
 
