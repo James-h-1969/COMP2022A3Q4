999a598ec68923b0c76c25f7ddd15b23553603ef
James Hocking <jhoc6907@uni.sydney.edu.au>
2023-10-11 16:10:13 +1100

    test

--- problem_4_1.py
+++ problem_4_1.py
@@ -7,7 +7,14 @@ cat "4.1.tm " | python problem_4_1.py
 ZERO = "0"
 ONE = "1"
 STAR = "*"
+UNDERLINE = "_"
 REPLACEMENT_LETTER = "Z"
+
+def checkifExists(lines, start_state, input):
+    for line in lines:
+        if start_state == line[0] and input == line[1]:
+            return False
+    return True
 
 if __name__ == '__main__':
     # read an TM via stdin. See parser.py for details on the returned object
@@ -15,19 +22,26 @@ if __name__ == '__main__':
     final_lines = []
     # Put your code here:
     counter = 10
+
     for line in lines:
         start_state, input_letter, output_letter, direction, output_state = line
+        
         if output_letter == ZERO or output_letter == STAR:
             if output_letter == ZERO:
                 final_lines.append([start_state, input_letter, REPLACEMENT_LETTER, direction, output_state])
             if output_letter == STAR:
                 if input_letter == STAR:
-                    final_lines.append([start_state, ZERO, REPLACEMENT_LETTER, direction, output_state])
-                    final_lines.append([start_state, ONE, ONE, direction, output_state])
+                    if checkifExists(final_lines, start_state, ZERO):
+                        final_lines.append([start_state, ZERO, REPLACEMENT_LETTER, direction, output_state])
+                    if checkifExists(final_lines, start_state, ONE):
+                        final_lines.append([start_state, ONE, ONE, direction, output_state])
+                    if checkifExists(final_lines, start_state, UNDERLINE):
+                        final_lines.append([start_state, UNDERLINE, UNDERLINE, direction, output_state])
                 if input_letter == ZERO:
                     final_lines.append([start_state, ZERO, REPLACEMENT_LETTER, direction, output_state])
         else:
             final_lines.append(line)
+
         if input_letter == ZERO:
             if output_letter == ZERO:
                 final_lines.append([start_state, REPLACEMENT_LETTER, REPLACEMENT_LETTER, direction, output_state])
