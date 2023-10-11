a343b59ce704cd3a9bf03546b24f8cb9add08c6c
James Hocking <jhoc6907@uni.sydney.edu.au>
2023-10-11 15:09:32 +1100

    updating

--- problem_4_1.py
+++ problem_4_1.py
@@ -7,45 +7,7 @@ cat "4.1.tm " | python problem_4_1.py
 ZERO = "0"
 ONE = "1"
 STAR = "*"
-
-def go_to_furthest(num:str, direction:str, final_state:str):
-    if (direction == "L" or direction == 'l'):
-        new_direction = "R"
-    else:
-        new_direction = "L"
-    final_lines = []
-    to_go = [num, "_", "_", new_direction, num+"a"]
-    to_end = [[num+"a", "0", "0", new_direction, num+"a"],
-              [num+"a", "1", "1", new_direction, num+"a"],
-              [num+"a", "2", "2", new_direction, num+"a"],
-              [num+"a", "3", "3", new_direction, num+"a"]]
-    actual_end = [num+"a", "_", "_", direction, num+"b"]
-    find_new_zero = get_to_a_zero(num+"b", direction, final_state, 1)
-    final_lines.append(to_go)
-    for line in to_end:
-        final_lines.append(line)
-    for line in find_new_zero:
-        final_lines.append(line)
-    final_lines.append(actual_end)
-    return final_lines
-
-def get_to_a_zero(num:str, direction:str, final_state:str, depth:int):
-    final_lines = []
-    keep_moving = [[num, "1", "1", direction, num], 
-                   [num, "2", "2", direction, num], 
-                   [num, "3", "3", direction, num], ]
-    if (depth == 0):
-        gets_to_end = go_to_furthest(num, direction, final_state)
-        for line in gets_to_end:
-            final_lines.append(line)
-    else:
-        final_lines.append([num, "_", "_", "L", "halt-reject"])
-    for line in keep_moving:
-        final_lines.append(line)
-    find_zero = [num, "0", "0", "*", final_state]
-    final_lines.append(find_zero)
-    return final_lines
-
+REPLACEMENT_LETTER = "Z"
 
 if __name__ == '__main__':
     # read an TM via stdin. See parser.py for details on the returned object
@@ -55,28 +17,19 @@ if __name__ == '__main__':
     counter = 10
     for line in lines:
         start_state, input_letter, output_letter, direction, output_state = line
-        if input_letter == ZERO or input_letter == STAR:
-            # follow direction
-            if input_letter == ZERO:            
-                final_lines.append([start_state, input_letter, output_letter, direction, str(counter)])
-                new_rules = get_to_a_zero(str(counter), direction, output_state, 0)
-                counter += 1
-                for line in new_rules:
-                    final_lines.append(line)
-            else:
-                final_lines.append([start_state, ZERO, ZERO, direction, str(counter)])
-                final_lines.append([start_state, ONE, ONE, direction, output_state])
-                new_rules = get_to_a_zero(str(counter), direction, output_state, 0)
-                counter += 1
-                for line in new_rules:
-                    final_lines.append(line)
+        if output_letter == ZERO or output_letter == STAR:
+            final_lines.append([start_state, input_letter, REPLACEMENT_LETTER, direction, output_state])
+            if output_letter == STAR:
+                final_lines.append([start_state, input_letter, ONE, direction, output_state])
         else:
             final_lines.append(line)
 
-
-    # if you use the same data structure, you can use:
+    
     # print("Initial TM:")
     # printTM(lines)
     # print(" ")
     # print("Final PTM:")
     printTM(final_lines)
+
+    
+
--- problem_4_2.py
+++ problem_4_2.py
@@ -1,10 +1,79 @@
-from TMparser import parseTM, printTM
+from TMparser import parseInput, printTM
+
+ZERO = "0"
+ONE = "1"
+STAR = "*"
+
+def go_to_furthest(num:str, direction:str, final_state:str):
+    if (direction == "L" or direction == 'l'):
+        new_direction = "R"
+    else:
+        new_direction = "L"
+    final_lines = []
+    to_go = [num, "_", "_", new_direction, num+"a"]
+    to_end = [[num+"a", "0", "0", new_direction, num+"a"],
+              [num+"a", "1", "1", new_direction, num+"a"],
+              [num+"a", "2", "2", new_direction, num+"a"],
+              [num+"a", "3", "3", new_direction, num+"a"]]
+    actual_end = [num+"a", "_", "_", direction, num+"b"]
+    find_new_zero = get_to_a_zero(num+"b", direction, final_state, 1)
+    final_lines.append(to_go)
+    for line in to_end:
+        final_lines.append(line)
+    for line in find_new_zero:
+        final_lines.append(line)
+    final_lines.append(actual_end)
+    return final_lines
+
+def get_to_a_zero(num:str, direction:str, final_state:str, depth:int):
+    final_lines = []
+    keep_moving = [[num, "1", "1", direction, num], 
+                   [num, "2", "2", direction, num], 
+                   [num, "3", "3", direction, num], ]
+    if (depth == 0):
+        gets_to_end = go_to_furthest(num, direction, final_state)
+        for line in gets_to_end:
+            final_lines.append(line)
+    # else:
+    #     final_lines.append([num, "_", "_", "L", "halt-reject"])
+    for line in keep_moving:
+        final_lines.append(line)
+    find_zero = [num, "0", "0", "*", final_state]
+    final_lines.append(find_zero)
+    return final_lines
+
 
 if __name__ == '__main__':
-    # read an PTM via stdin. See parser.py for details on the returned object
-    lines = parseTM()
-
+    # read an TM via stdin. See parser.py for details on the returned object
+    lines = parseInput()
+    final_lines = []
     # Put your code here:
+    counter = 10
+    for line in lines:
+        start_state, input_letter, output_letter, direction, output_state = line
+        if input_letter == ZERO or input_letter == STAR:
+            # follow direction
+            if input_letter == ZERO:            
+                final_lines.append([start_state, input_letter, output_letter, direction, str(counter)])
+                new_rules = get_to_a_zero(str(counter), direction, output_state, 0)
+                counter += 1
+                for line in new_rules:
+                    final_lines.append(line)
+            else:
+                final_lines.append([start_state, ZERO, ZERO, direction, str(counter)])
+                final_lines.append([start_state, ONE, ONE, direction, output_state])
+                new_rules = get_to_a_zero(str(counter), direction, output_state, 0)
+                counter += 1
+                for line in new_rules:
+                    final_lines.append(line)
+        else:
+            final_lines.append(line)
+
 
     # if you use the same data structure, you can use:
+    print("Initial TM:")
     printTM(lines)
+    print(" ")
+    print("Final PTM:")
+    printTM(final_lines)
+
